405fb72947c3ca22648b05492a09c35d9f8959eb
Matthew Chee <matthew.chee@student.manchester.ac.uk>
2024-05-26 14:30:10 +0100

    Added card and deck class

---
+++ deck.py
@@ -0,0 +1,38 @@
+import random
+
+class Card:
+    def __init__(self,value,name,suit):
+        self.value = value
+        self.name = name
+        self.suit = suit
+
+    def __repr__(self):
+        return f"{self.name} of {self.suit}"
+
+import random
+
+class Deck:
+    suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
+    names = ['Ace','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King']
+    values = range(1, 14)
+
+    def __init__(self, numDecks=1):
+        self.cards = []
+        for _ in range(numDecks):
+            for suit in self.suits:
+                for index,name in enumerate(self.names):
+                    if index < 9:
+                        self.cards.append(Card(index+1,name,suit))
+                    else:
+                        self.cards.append(Card(10,name,suit))
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def deal(self):
+        return self.cards.pop() if self.cards else None
+
+    def __repr__(self):
+        return f"Deck of {len(self.cards)} cards"
+
+
---
+++ main.py
@@ -0,0 +1,11 @@
+from deck import Deck
+
+def main():
+    deck = Deck(1)
+    print(deck.cards)
+    print(len(deck.cards))
+
+
+
+if __name__ == "__main__":
+    main()
